bc40ffd1bbac021dc4731d6047654035fe7cf25c
Harsh Rohila <rohilaharsh@gmail.com>
2020-03-21 19:29:31 +0530

    added body-parser and post handler

--- express-practice/src/app.js
+++ express-practice/src/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+var bodyParser = require('body-parser')
+
 const app = express();
+
+app.use(bodyParser.json())
 
 app.get('/', (_req, res) => {
      res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
      const response = { message: 'Hello World!' };
+     res.send(response);
+});
+
+app.post('/user', (req, res) => {
+     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+     console.log(req.body);
+     const response = { message: 'Got it!' };
      res.send(response);
 });
 
