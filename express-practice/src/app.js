d642b567b03a79b2fa032ba0d620f6f4f20cd646
Harsh Rohila <rohilaharsh@gmail.com>
2020-03-21 19:36:31 +0530

    added cors middleware for express

--- express-practice/src/app.js
+++ express-practice/src/app.js
@@ -1,18 +1,23 @@
 const express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+     next();
+})
 
 app.get('/', (_req, res) => {
-     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
      const response = { message: 'Hello World!' };
      res.send(response);
 });
 
 app.post('/user', (req, res) => {
-     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
      console.log(req.body);
      const response = { message: 'Got it!' };
      res.send(response);
